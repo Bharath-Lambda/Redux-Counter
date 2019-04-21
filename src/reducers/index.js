46a6aaa375370b1a765533dd0e4efdaf465f9f7b
Bharath <b.lavendra.95@gmail.com>
2019-04-21 11:56:19 -0500

    Functional!

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,34 +1,21 @@
-// import { INCREMENT, DECREMENT } from '../actions';
-
-// const initialState = {
-//   count: 0
-// }
-
-// // Our reducer that handles our two action cases:
-// // increment and decrement. It receives the state
-// // of our redux store, along with an action created
-// // by our action creator. What does the reducer
-// // need to do with the count in each case?
-// export default (count = 0, action) => {
-//   switch (action.type) {
-//     case INCREMENT:
-//       return ++count
-//     case DECREMENT:
-//       return --count;
-//     default:
-//       return count;
-//   }
-// };
-
 import { INCREMENT, DECREMENT } from '../actions';
 
-export default (count = 0, action) => {
-    switch (action.type) {
-        case INCREMENT:
-            return ++count;
-        case DECREMENT:
-            return --count;
-        default:
-            return count
-    }
+const initialState = {
+  count: 0
+}
+
+// Our reducer that handles our two action cases:
+// increment and decrement. It receives the state
+// of our redux store, along with an action created
+// by our action creator. What does the reducer
+// need to do with the count in each case?
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {count: ++state.count}
+    case DECREMENT:
+      return {count: --state.count}
+    default:
+      return state;
+  }
 };
